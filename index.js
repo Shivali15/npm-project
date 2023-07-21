b782b754a1744fb0ed8756610ba100ed2779691f
shivali umbarkar <shivali.umbarkar@konverge.ai>
2023-07-21 14:22:27 +0530

    Adding npm package

--- index.js
+++ index.js
@@ -1,50 +1,5 @@
-import inquirer from "inquirer";
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "strategy",
-      message: "Which authentication strategy do you want to use?",
-      choices: ["JWT Strategy", "AWS Cognito", "Auth0", "Google"],
-    },
-    {
-      type: "confirm",
-      name: "twoFA",
-      message: "Do you want to add 2FA?",
-      choices: ["YES", "NO"],
-      default: true,
-      when(answers) {
-        return answers.strategy === "JWT Strategy";
-      },
-    },
-    {
-      type: "confirm",
-      name: "captcha",
-      message: "Do you want to add captcha?",
-      choices: ["YES", "NO"],
-      when(answers) {
-        return answers.strategy === "JWT Strategy";
-      },
-    },
-  ])
-  .then(function (selAns) {
-    const answers = selAns;
-    console.log(answers.strategy);
-    console.log(answers.twoFA);
-    console.log(answers.captcha);
-  });
+console.log("Hello from our very own npm package.")
 
-// const handlebuttonclick = (answers) => {
-//   return console.log(answers.strategy);
-// };
+const sayMyName = name => console.log(`Your name is ${name}.`)
 
-// const sayMyName = () => {
-//   return handlebuttonclick();
-//   // <div>
-//   //   <h1>Hello Nature</h1>
-//   //   <button onClick={handlebuttonclick()}>Submit</button>
-//   //   <span>{strategy}</span>
-//   // </div>
-// };
-
-// module.exports = { sayMyName };
+module.exports = {sayMyName}
